ac7846021cf3424cdd5e2a7d8b075767ed52cda1
Brian K. Christensen <bkc@secoya.dk>
2017-11-02 10:49:54 +0100

    Environment: implemented create logger from environment

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,5 +1,6 @@
+import * as clc from 'cli-color';
 import { defaultLogLevel, AnsiLogger, Level } from '../AnsiLogger';
-import { createJSONLogger, createTextLogger } from '../index';
+import { createJSONLogger, createLoggerFromEnvironment, createTextLogger } from '../index';
 import { JSONTransformer } from '../JSONTransformer';
 import { TextTransformer } from '../TextTransformer';
 
@@ -40,5 +41,97 @@ describe('createJSONLogger', () => {
 	test('log level options is passed along to the logger', () => {
 		const logger = createJSONLogger({ logLevel: Level.DEBUG });
 		expect(logger.options.logLevel).toBe(Level.DEBUG);
+	});
+});
+
+describe('createLoggerFromEnvironment', () => {
+	const oldEnv = process.env;
+	afterEach(() => {
+		process.env = oldEnv;
+	});
+
+	test('log level from options is used if env is empty', () => {
+		process.env = {};
+		let logger;
+		logger = createLoggerFromEnvironment({ logLevel: Level.SILENT });
+		expect(logger.options.logLevel).toBe(Level.SILENT);
+		logger = createLoggerFromEnvironment({ logLevel: Level.ERROR });
+		expect(logger.options.logLevel).toBe(Level.ERROR);
+		logger = createLoggerFromEnvironment({ logLevel: Level.WARN });
+		expect(logger.options.logLevel).toBe(Level.WARN);
+		logger = createLoggerFromEnvironment({ logLevel: Level.SUCCESS });
+		expect(logger.options.logLevel).toBe(Level.SUCCESS);
+		logger = createLoggerFromEnvironment({ logLevel: Level.LOG });
+		expect(logger.options.logLevel).toBe(Level.LOG);
+		logger = createLoggerFromEnvironment({ logLevel: Level.INFO });
+		expect(logger.options.logLevel).toBe(Level.INFO);
+		logger = createLoggerFromEnvironment({ logLevel: Level.DEBUG });
+		expect(logger.options.logLevel).toBe(Level.DEBUG);
+		logger = createLoggerFromEnvironment({ logLevel: Level.VERBOSE });
+		expect(logger.options.logLevel).toBe(Level.VERBOSE);
+		// tslint:disable:no-bitwise
+		logger = createLoggerFromEnvironment({ logLevel: Level.ERROR | Level.INFO });
+		expect(logger.options.logLevel).toBe(Level.ERROR | Level.INFO);
+		// tslint:enable:no-bitwise
+	});
+
+	test('log level from environment overrides log level from options', () => {
+		let logger;
+
+		process.env = { LOGLEVEL: 'ERROR' } as any;
+		logger = createLoggerFromEnvironment({ logLevel: Level.SILENT });
+		expect(logger.options.logLevel).toBe(Level.ERROR);
+
+		process.env = { LOGLEVEL: 'SILENT' } as any;
+		logger = createLoggerFromEnvironment({ logLevel: Level.VERBOSE });
+		expect(logger.options.logLevel).toBe(Level.SILENT);
+	});
+
+	test('log level from environment works with numbers and level enum values', () => {
+		let logger;
+
+		process.env = { LOGLEVEL: 127 } as any;
+		logger = createLoggerFromEnvironment();
+		expect(logger.options.logLevel).toBe(Level.VERBOSE);
+	});
+
+	test('log format from options', () => {
+		let logger;
+		logger = createLoggerFromEnvironment({ logFormat: 'TEXT' });
+		expect(logger.options.transformer).toBeInstanceOf(TextTransformer);
+		logger = createLoggerFromEnvironment({ logFormat: 'JSON' });
+		expect(logger.options.transformer).toBeInstanceOf(JSONTransformer);
+	});
+
+	test('log format from environment overrides options', () => {
+		let logger;
+
+		process.env = { LOGFORMAT: 'TEXT' } as any;
+		logger = createLoggerFromEnvironment({ logFormat: 'JSON' });
+		expect(logger.options.transformer).toBeInstanceOf(TextTransformer);
+		process.env = { LOGFORMAT: 'JSON' } as any;
+		logger = createLoggerFromEnvironment({ logFormat: 'TEXT' });
+		expect(logger.options.transformer).toBeInstanceOf(JSONTransformer);
+	});
+
+	test('when creating a text logger from the group color is passed', () => {
+		let logger;
+		logger = createLoggerFromEnvironment({ groupColor: clc.red });
+		expect((logger.options.transformer as TextTransformer).colors.GROUP).toBe(clc.red);
+
+		logger = createLoggerFromEnvironment({ groupColor: clc.green });
+		expect((logger.options.transformer as TextTransformer).colors.GROUP).toBe(clc.green);
+	});
+
+	test('when creating a logger from environment the group is passed along', () => {
+		let logger;
+		logger = createLoggerFromEnvironment();
+		expect(logger.options.group).toBeUndefined();
+
+		logger = createLoggerFromEnvironment({ group: 'test', logFormat: 'TEXT' });
+		expect(logger.options.group).toBe('test');
+
+		logger = createLoggerFromEnvironment({ group: 'test', logFormat: 'JSON' });
+		expect(logger.options.group).toBe('test');
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from './TextTransformer';
 export * from './IdentityTransformer';
 
 import { Format } from 'cli-color';
-import { AnsiLogger } from './AnsiLogger';
+import { defaultLogLevel, AnsiLogger, Level, LoggerOptions, Transformer } from './AnsiLogger';
 import { JSONTransformer } from './JSONTransformer';
 import { TextTransformer } from './TextTransformer';
 
@@ -44,4 +44,73 @@ export function createJSONLogger(options?: { group?: string; logLevel?: number }
 		logLevel: opts.logLevel,
 		transformer: new JSONTransformer(),
 	});
+}
+
+function resolveLogLevel(levelOrMask?: number | keyof typeof Level): Level {
+	switch (levelOrMask) {
+		case 'SILENT':
+			return Level.SILENT;
+		case 'ERROR':
+			return Level.ERROR;
+		case 'WARN':
+			return Level.WARN;
+		case 'SUCCESS':
+			return Level.SUCCESS;
+		case 'LOG':
+			return Level.LOG;
+		case 'INFO':
+			return Level.INFO;
+		case 'DEBUG':
+			return Level.DEBUG;
+		case 'VERBOSE':
+			return Level.VERBOSE;
+		case undefined:
+			return defaultLogLevel;
+		default:
+			return parseInt(levelOrMask as any, 10);
+	}
+}
+
+/**
+ * Create logger based on the options passed,
+ * but let environment variables overwrite the options
+ * if they are defeined.
+ *
+ * Default is TEXT logger with log level INFO.
+ *
+ * The environment variables are:
+ * - LOGLEVEL   possible values are: SILENT | ERROR | WARN | SUCCESS | LOG | INFO | DEBUG | VERBOSE | number
+ * - LOGFORMAT  possible values are: TEXT | JSON
+ */
+export function createLoggerFromEnvironment(options?: {
+	group?: string;
+	groupColor?: Format;
+	logFormat?: 'TEXT' | 'JSON';
+	logLevel?: number | Level;
+}): AnsiLogger<Transformer<any>> {
+	const opts = options == null ? {} : options;
+
+	const loggerOptions: Partial<LoggerOptions<any>> = {};
+
+	const envLogLevel = process.env.LOGLEVEL;
+	const envLogFormat =
+		['TEXT', 'JSON'].indexOf(process.env.LOGFORMAT as string) !== -1 ? process.env.LOGFORMAT : null;
+
+	const logLevel = envLogLevel == null ? opts.logLevel : resolveLogLevel(envLogLevel as number | keyof typeof Level);
+	const logFormat = envLogFormat == null ? opts.logFormat : envLogFormat;
+
+	if (logLevel != null) {
+		loggerOptions.logLevel = logLevel;
+	}
+
+	return logFormat === 'JSON'
+		? createJSONLogger({
+				group: opts.group,
+				logLevel: loggerOptions.logLevel,
+			})
+		: createTextLogger({
+				group: opts.group,
+				groupColor: opts.groupColor,
+				logLevel: loggerOptions.logLevel,
+			});
 }
