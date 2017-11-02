37778b77950ff36196bd7bdbf83ac28c926ba116
Brian K. Christensen <bkc@secoya.dk>
2017-11-02 08:51:57 +0100

    Text: add group color option to text logger factory

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './JSONTransformer';
 export * from './TextTransformer';
 export * from './IdentityTransformer';
 
+import { Format } from 'cli-color';
 import { AnsiLogger } from './AnsiLogger';
 import { JSONTransformer } from './JSONTransformer';
 import { TextTransformer } from './TextTransformer';
@@ -12,19 +13,31 @@ import { TextTransformer } from './TextTransformer';
  * Create a simple text logger, generally used to output human readable
  * format to the console.
  */
-export function createTextLogger(options?: { group?: string, logLevel?: number }): AnsiLogger<TextTransformer> {
+export function createTextLogger(options?: {
+	group?: string;
+	groupColor?: Format;
+	logLevel?: number;
+}): AnsiLogger<TextTransformer> {
 	const opts = options == null ? {} : options;
 	return new AnsiLogger({
 		group: opts.group,
 		logLevel: opts.logLevel,
-		transformer: new TextTransformer(),
+		transformer: new TextTransformer(
+			opts.groupColor == null
+				? undefined
+				: {
+						colorMap: {
+							GROUP: opts.groupColor,
+						},
+					},
+		),
 	});
 }
 /**
  * Create simple JSON logger, generally used for when outputting
  * to log services like e.g. Log Stash.
  */
-export function createJSONLogger(options?: { group?: string, logLevel?: number }): AnsiLogger<JSONTransformer> {
+export function createJSONLogger(options?: { group?: string; logLevel?: number }): AnsiLogger<JSONTransformer> {
 	const opts = options == null ? {} : options;
 	return new AnsiLogger({
 		group: opts.group,
